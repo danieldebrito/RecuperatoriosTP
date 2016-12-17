8ff3dac1e862ad2e3eaa8d2f2a6e1484956e0bae
Daniel Ruben de Brito <danieldebrito@outlook.com>
2016-12-16 23:28:43 -0300

    TP4
    TP4

---
+++ tp4/Hilo/Descargador.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Net; // Avisar del espacio de nombre
+using System.ComponentModel;
+
+namespace Hilo
+{
+    public class Descargador
+    {
+        private Uri direccion;
+        private string resultado;
+
+        public delegate void Progress(int progreso);
+        public delegate void DownloadComplete(string html);
+
+        public event Progress progreso;
+        public event DownloadComplete DescargaCompleta;
+
+        public Descargador(Uri direccion)
+        {
+            this.direccion = direccion;
+        }
+
+        public void IniciarDescarga()
+        {
+            try
+            {
+                WebClient cliente = new WebClient();
+                cliente.DownloadProgressChanged += WebClientDownloadProgressChanged;
+                cliente.DownloadStringCompleted += WebClientDownloadCompleted;
+                cliente.DownloadStringAsync(direccion);
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+
+        private void WebClientDownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            this.progreso(e.ProgressPercentage);
+        }
+
+        private void WebClientDownloadCompleted(object sender, DownloadStringCompletedEventArgs e)
+        {
+            try
+            {
+                this.resultado = e.Result;
+            }
+            catch (Exception)
+            { 
+
+            }
+            
+            this.DescargaCompleta(this.resultado);
+        }
+    }
+}
